06d8f4c876ca3611aea33611ca915f7752f6c7a5
Michael Myers <myersmyer@gmail.com>
2018-05-20 15:18:25 -0400

    added stuff

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friendsGroup = [
+    {
+        name: 'Yoyo Ma',
+        profilePic: 'http://www.yo-yoma.com/wp-content/uploads/2015/06/1986_4_8-YYM-with-Elmo-edited-800x590.jpg',
+        answers: [
+            '4',
+            '3',
+            '2',
+            '1',
+            '5',
+            '4',
+            '3',
+            '2',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Kenny G',
+        profilePic: 'http://media.pennlive.com/go/photo/kennyg-hires-4x6jpg-f5bcca4c6e8a90af.jpg',
+        answers: [
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '1',
+            '2',
+            '3',
+            '4',
+            '5'
+        ]
+    }
+]
+
+module.exports = friendsGroup;
